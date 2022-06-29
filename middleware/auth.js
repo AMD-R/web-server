8ce57ea4150f67a691ef2f2e221662954a452e3c
ecyht2 <ecyht2@nottingham.edu.my>
2022-06-29 20:57:10 +0800

    Moved jwtSecret

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,5 @@
 const jwt = require("jsonwebtoken");
-const jwtSecret =
-  "4715aed3c946f7b0a38e6b534a9583628d84e96d10fbc04700770d572af3dce43625dd";
+const { jwtSecret } = require('../config.json');
 
 exports.adminAuth = (req, res, next) => {
   const token = req.cookies.jwt;
