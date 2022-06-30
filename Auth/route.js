a5f5dd1930b7e781fb8dc8edb8c3bfa63edbd062
ecyht2 <ecyht2@nottingham.edu.my>
2022-06-30 14:13:13 +0800

    Added API auth checks

--- Auth/route.js
+++ Auth/route.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const { register, login, update, deleteUser, getUsers } = require("./auth");
-const { adminAuth } = require("../middleware/auth");
+const { adminAuthAPI } = require("../middleware/auth");
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-router.route("/update").put(adminAuth, update);
-router.route("/deleteUser").delete(adminAuth, deleteUser);
+router.route("/update").put(adminAuthAPI, update);
+router.route("/deleteUser").delete(adminAuthAPI, deleteUser);
 router.route("/getUsers").get(getUsers);
 
 module.exports = router;
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const { jwtSecret } = require('../config.json');
 
+// Normal middleware
 exports.adminAuth = (req, res, next) => {
   const token = req.cookies.jwt;
   if (token) {
@@ -39,5 +40,47 @@ exports.userAuth = (req, res, next) => {
     return res
       .status(401)
       .render('error/401');
+  }
+};
+
+// API middleware
+exports.adminAuthAPI = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        return res.status(401).json({message: 'Permissions Denied'});
+      } else {
+        if (decodedToken.role !== "admin") {
+          return res.status(401).json({message: 'Permissions Denied'});
+        } else {
+          next();
+        }
+      }
+    });
+  } else {
+    return res
+      .status(401)
+      .json({message: 'Permissions Denied'});
+  }
+};
+exports.userAuthAPI = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        return res.status(401).json({message: 'Permissions Denied'});
+      } else {
+        if (decodedToken.role !== "Basic") {
+          return res.status(401).json({message: 'Permissions Denied'});
+        } else {
+          next();
+        }
+      }
+    });
+  } else {
+    return res
+      .status(401)
+      .json({message: 'Permissions Denied'});
   }
 };
