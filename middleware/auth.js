bbf9ac0c1213cb74c132df3c6650fd5ee3fe95ec
ecyht2 <ecyht2@nottingham.edu.my>
2022-07-07 14:54:56 +0800

    Added jsdoc for auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,15 @@
 const jwt = require("jsonwebtoken");
 const { jwtSecret } = require('../config.json');
+const amdr = require('../model/AMD-R');
+const bcrypt = require("bcryptjs");
+const { request, response } = require('express');
 
 // Normal middleware
+/**
+ * Check if the user logged in is an admin
+ * @param {request} req
+ * @param {response} res
+ * */
 exports.adminAuth = (req, res, next) => {
   const token = req.cookies.jwt;
   if (token) {
@@ -22,6 +30,11 @@ exports.adminAuth = (req, res, next) => {
       .render('error/401');
   }
 };
+/**
+ * Check if the user logged in is a normal user
+ * @param {request} req
+ * @param {response} res
+ * */
 exports.userAuth = (req, res, next) => {
   const token = req.cookies.jwt;
   if (token) {
@@ -44,6 +57,11 @@ exports.userAuth = (req, res, next) => {
 };
 
 // API middleware
+/**
+ * Check if the user logged in is an admin (API version)
+ * @param {request} req
+ * @param {response} res
+ * */
 exports.adminAuthAPI = (req, res, next) => {
   const token = req.cookies.jwt;
   if (token) {
@@ -64,6 +82,11 @@ exports.adminAuthAPI = (req, res, next) => {
       .json({message: 'Permissions Denied'});
   }
 };
+/**
+ * Check if the user logged in is a normal user (API version)
+ * @param {request} req
+ * @param {response} res
+ * */
 exports.userAuthAPI = (req, res, next) => {
   const token = req.cookies.jwt;
   if (token) {
