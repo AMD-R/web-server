aec54b600bcfaef0047b849e3752e8160fd3eb4b
ecyht2 <ecyht2@nottingham.edu.my>
2022-08-16 14:53:44 +0800

    Updated getOTP to apply for all users

--- Auth/route.js
+++ Auth/route.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const { register, login, update, deleteUser, getUsers, getOTP } = require('./auth');
-const { adminAuthAPI, userAuthAPI } = require('../middleware/auth');
+const { adminAuthAPI, sessionAuthAPI } = require('../middleware/auth');
 
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/update').put(adminAuthAPI, update);
 router.route('/deleteUser').delete(adminAuthAPI, deleteUser);
 router.route('/getUsers').get(getUsers);
-router.route('/getOTP').get(userAuthAPI, getOTP);
+router.route('/getOTP').get(sessionAuthAPI, getOTP);
 
 module.exports = router;
